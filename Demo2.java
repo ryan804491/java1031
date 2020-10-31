3b10f3bc482e6e397400f4830afa52baaaaad023
ryan804491 <fieldofgod3@gmail.com>
2020-10-31 21:19:21 +0800

    my first java

---
+++ Demo2.java
@@ -0,0 +1,29 @@
+public class Demo2 {
+    public static void main(String[] args) {
+        /*String ryan1 = "hello";
+        String ryan2 = "world";
+        String ryan3 =ryan1 +" "+ ryan2;
+        System.out.println(ryan3);
+
+         */
+        /*
+        String x = "我很好";
+        String y = "我好嗎";
+        String z = "隨便啦";
+        System.out.printf("今天好熱%S%S%S",x,y,z);
+        */
+        /*String letterLength = "一句話簡介，這個是一個被渣男和渣女算計之後，" +
+                "奮起反擊，報復過後，卻意外重生，活出錦繡人生，收穫真愛的故事。" +
+                "蕭家嫡女，風華絕代，妖嬈嫵媚，癡戀太子。二人郎才女貌，乃是天作之合" +
+                "。十年夫妻，蕭紫語殫精極慮，...".length();
+        System.out.println("一句話簡介，這個是一個被渣男和渣女算計之後， 奮起反擊，報復過後，卻意外重生，活出錦繡人生，收穫真愛的故事。 蕭家嫡女，風華絕代，妖嬈嫵媚，癡戀太子。二人郎才女貌，乃是天作之合。十年夫妻，蕭紫語殫精極慮，...".length());*/
+        /*int intval = 100;
+        String Strval = Integer.toString(intval);
+        System.out.println(Strval);*/
+
+
+
+
+
+    }
+}
